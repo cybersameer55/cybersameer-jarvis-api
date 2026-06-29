a5bc87208f13f879247e702f5ce472f0e3170334
CyberSameer <cybersameertemp4@gmail.com>
2026-06-29 10:16:52 +0530

    Add files via upload

--- cybersameer_apis/num.py
+++ cybersameer_apis/num.py
@@ -26,21 +26,24 @@ def num():
 
     n = quote(num_val)
 
+    # ---------- 4 APIs (as requested) ----------
     api1 = clean_response(fetch_api(f"https://api.b77bf911.workers.dev/mobile?number={n}") or {})
     api2 = clean_response(fetch_api(f"https://calltracerinfoapi.vercel.app/api?number={n}") or {})
     api3 = clean_response(fetch_api(f"https://ft-osint-api.duckdns.org/api/numleak?key=freetill1&num={n}") or {})
     api4 = clean_response(fetch_api(f"https://anon-num-info.vercel.app/num?key=tnum1906&num={n}") or {})
 
-    if not api2 and not api7 and not api8 and not api9:
+    # ---------- Check ----------
+    if not api1 and not api2 and not api3 and not api4:
         return send_response("error", {}, {"message": "No data found"})
 
+    # ---------- Response ----------
     return send_response("success", {
         "input_number": num_val,
         "time": get_time_now(),
         "sameer_lookup": {
-            "api_1": make_list(api2),
-            "api_2": make_list(api7),
-            "api_3": make_list(api8),
-            "api_4": make_list(api9)
+            "api_1": make_list(api1),
+            "api_2": make_list(api2),
+            "api_3": make_list(api3),
+            "api_4": make_list(api4)
         }
     }, {"user": user["name"]})
