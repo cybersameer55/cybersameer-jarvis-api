bd9ce2d90132ea3850817741f3cf72246a0215f2
CyberSameer <cybersameertemp4@gmail.com>
2026-06-29 21:32:29 +0530

    Update core.py

--- core.py
+++ core.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.DEBUG)
 USERS = {
     "BHAI":              {"name": "𝐇𝐀𝐂𝐊𝐄𝐑 𝐇𝐔 𝐁𝐇𝐀𝐈",             "expiry": "unlimited", "status": "active"},
     "BHAIYA":            {"name": "𝐑𝐀𝐃𝐇𝐄 𝐁𝐇𝐀𝐈𝐘𝐀",              "expiry": "unlimited", "status": "active"},
-    "MITRO":             {"name": "𝐒𝐈𝐑𝐅 𝐃𝐎𝐒𝐓𝐎 𝐊𝐄 𝐋𝐈𝐘𝐄",       "expiry": "2026-6-29", "status": "active"},
+    "MITRO":             {"name": "𝐒𝐈𝐑𝐅 𝐃𝐎𝐒𝐓𝐎 𝐊𝐄 𝐋𝐈𝐘𝐄",       "expiry": "2026-7-29", "status": "active"},
     "SAMEER":            {"name": "𝐒𝐈𝐑𝐅 𝐃𝐎𝐒𝐓𝐎 𝐊𝐄 𝐋𝐈𝐘𝐄",       "expiry": "2026-6-29", "status": "active"},
     "Anonymous":         {"name": "𝐅𝐑𝐈𝐄𝐍𝐃 𝐇𝐀𝐈 𝐌𝐄𝐑𝐀",          "expiry": "2026-6-29", "status": "active"},
     "DARKBHIYA":         {"name": "𝐃𝐀𝐑𝐊 𝐁𝐇𝐀𝐈𝐘𝐀",                "expiry": "2026-7-29", "status": "active"},
